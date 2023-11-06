af31824653b34efe8cf0b3db0802d9c53066468a
Jorge Jiménez García <62335048+Axemt@users.noreply.github.com>
2023-11-06 09:10:42 +0100

    Added cleanup procedure to tests
    This helps remove local size due to leftover docker images from testing

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,13 @@
+# Build and install
 python3 -m pip install .
 
-
+# Run tests with coverage
 coverage run --source away/ -m unittest discover -s tests -p '*.py'
 
+# Cleanup
+rm -rf build/
+docker system prune -a --force
+minikube ssh -- docker system prune -a --force
 
+# Report
 coverage report -m --fail-under 80

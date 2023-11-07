bc833e629b3afd5331481a092f8ebe05f80843cf
Jorge Jiménez García <62335048+Axemt@users.noreply.github.com>
2023-11-07 16:38:43 +0100

    Added tests for stateful checks

---
+++ tests/checks_stateless.py
@@ -0,0 +1,28 @@
+from away import builder, FaasConnection
+
+faas = FaasConnection(password=1234)
+
+class T:
+
+    def static_fn_part_of_class(n):
+        return n+1
+    
+    def class_method(self, n):
+        return n+1
+
+import unittest
+class TestStateless(unittest.TestCase):
+
+    def test_with_instance_raises(self):
+
+        self.assertRaises(Exception, builder.mirror_in_faas, T().static_fn_part_of_class, faas)
+        self.assertRaises(Exception, builder.mirror_in_faas, T().class_method, faas)
+
+    def test_from_uninstanced_doesnt_take_self(self):
+
+        static_fn = builder.mirror_in_faas(T.static_fn_part_of_class, faas, verbose=True)
+        self.assertEqual(static_fn(1), 2)
+
+    def test_from_uninstanced_takes_self(self):
+
+        self.assertRaises(Exception, builder.mirror_in_faas, T.class_method, faas)

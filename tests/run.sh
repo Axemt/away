d6a5f2f8f80f3c8451a3841ba1d298cbf27f8194
Jorge Jiménez García <62335048+Axemt@users.noreply.github.com>
2023-11-08 10:05:30 +0000

    Simplified test runner script
    Now no longer requires strict mode. Also runs cleanups silently:

--- tests/run.sh
+++ tests/run.sh
@@ -1,16 +1,14 @@
-# Fail stage if any command in script fails
-set -e
-
 # Build and install
 python3 -m pip install .
 
 # Run tests with coverage
 coverage run --source away/ -m unittest discover -s tests -p '*.py'
+test_retcode=$?
 
 # Cleanup
 rm -rf build/
-docker system prune -a --force --volumes
-minikube ssh -- docker system prune -a --force --volumes
+docker system prune -a --force --volumes >> /dev/null
+minikube ssh -- docker system prune -a --force --volumes >> /dev/null
 
 # Report
 coverage report -m --fail-under 80
